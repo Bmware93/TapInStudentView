a13813b071a13e7642ab71626a5834eda9a546a4
Benia Morgan <bmorganware@gmail.com>
2023-02-19 16:17:15 -0500

    Initial Commit

---
+++ TapInStudentLogin/TapInStudentLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapInStudentLoginApp.swift
+//  TapInStudentLogin
+//
+//  Created by Benia Morgan-Ware on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TapInStudentLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
